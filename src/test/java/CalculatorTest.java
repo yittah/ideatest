f50e300ff91a1056f8a1b5df505c5b70a9031414
yittah <yittah@163.com>
2021-03-28 13:25:03 +0800

    refactor

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,17 +5,17 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
     @Test
-    public void shouldReturn1when1plus1(){
+    public void shouldReturn1when1plus1() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         Calculator calculator = new Calculator();
-        int actualResult = calculator.compute(1, 1, '+');
+        int actualResult = calculator.compute(1, 1, "Add");
         int expectResult = 2;
         Assert.assertEquals(expectResult,actualResult);
     }
 
     @Test
-    public void shouldReturn0when1sub1(){
+    public void shouldReturn0when1sub1() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         Calculator calculator = new Calculator();
-        int actualResult = calculator.compute(1, 1, '-');
+        int actualResult = calculator.compute(1, 1, "Sub");
         int expectResult = 0;
         Assert.assertEquals(expectResult,actualResult);
     }
