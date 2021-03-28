d5eb6c312f703bf5be4e25bdd364bc2aabece35d
yittah <yittah@163.com>
2021-03-28 12:55:06 +0800

    add method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,12 @@
 public class Calculator {
     public int compute(int num1, int num2, char symbol) {
-        return 1;
+        switch (symbol){
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
